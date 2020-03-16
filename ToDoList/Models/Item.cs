3a81b2bf754addd06e471745fe933dcdd9da3901
Drake Wilcox <drakewilcox@gmail.com>
2020-03-16 13:04:26 -0700

    clean up code and ran into server error so homework was no longer able to be accessed. Co-authored-by: Jeremy-Kale-Padot <jeremy@padot.us>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -43,7 +43,8 @@ namespace ToDoList.Models
 
     public static void ClearAll()
     {
-  
+      MySqlConnection conn = DB.Connection();
+      conn.Open(); 
     }
 
     public static Item Find(int searchId)
